5b29f6de34c387ce9cc95b42182057e636032b7d
lakshay123321 <lakshay.ahujarock1@gmail.com>
2024-03-26 19:02:46 +0530

    Fifth Commit

--- force-app/main/default/lwc/p2CAndApiDecorator/p2CAndApiDecorator.js
+++ force-app/main/default/lwc/p2CAndApiDecorator/p2CAndApiDecorator.js
@@ -2,5 +2,10 @@ import { LightningElement ,api} from 'lwc';
 
 export default class P2CAndApiDecorator extends LightningElement {
 
-   @api myTitle = 'Lakshay Ahuja';
+     @api myTitle = 'Lakshay Ahuja';
+
+     @api handleValue()
+     {
+        this.myTitle='Lakshay Ahuja is chnaged to Lakshay';
+     }
 }
--- force-app/main/default/lwc/p2CAndApiDecoratorParent/p2CAndApiDecoratorParent.js
+++ force-app/main/default/lwc/p2CAndApiDecoratorParent/p2CAndApiDecoratorParent.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class P2CAndApiDecoratorParent extends LightningElement {}
+export default class P2CAndApiDecoratorParent extends LightningElement {
+
+   handleValueChange()
+   {
+    this.template.querySelector('c-p2-c-and-api-decorator').handleValue();
+   }
+}
