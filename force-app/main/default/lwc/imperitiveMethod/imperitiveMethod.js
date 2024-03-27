2dbf77a5e9f8f1669d8a227de52ec2adcbd37921
lakshay123321 <lakshay.ahujarock1@gmail.com>
2024-03-27 13:38:09 +0530

    Eigth Commit

---
+++ force-app/main/default/lwc/imperitiveMethod/imperitiveMethod.js
@@ -0,0 +1,24 @@
+import { LightningElement ,track} from 'lwc';
+import getContactList from '@salesforce/apex/ContactInfo.getContactList';
+const columns =[
+    {label : 'Contact Record Id' , fieldName : 'Id'},
+    {label : 'Contact Name' , fieldName : 'Name'}
+];
+export default class ImperitiveMethod extends LightningElement {
+    @track columns =columns;
+    @track data = [];
+    @track error ;
+
+    connectedCallback()
+    {
+        getContactList()
+            .then(result=>
+                {
+                    this.data=result;
+                })
+            .catch(error=>{
+                this.error = error;
+            })     
+    }
+
+}
